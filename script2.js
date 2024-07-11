026eac3ed0fc0c0f5ad1afbc7fc378e2f40f1b10
Swagnik Ghosh <swagnikghosh705@gmail.com>
2024-07-12 02:45:43 +0530

    first commit

---
+++ script2.js
@@ -0,0 +1,16 @@
+
+let conn=document.querySelectorAll(".connction-info a");
+let row=document.querySelectorAll(".row p");
+row[0].innerText="C 101 Ramdashati GardenReach Kolkata 700024";
+row[1].innerText="+91 891062352";
+row[2].innerText="mrinalkantitripathi@gmail.com";
+conn[0].innerText="mrinalkantitripathi@gmail.com";
+conn[0].style.marginLeft="-36px";
+
+conn[1].innerText="+91 891062352";
+let conn2=document.querySelector(".connction-info p");
+conn2.innerText="C 101 Ramdashati GardenReach Kolkata 700024";
+let notuseful=document.querySelectorAll(".notuseful");
+notuseful.style.display = 'none';
+
+
